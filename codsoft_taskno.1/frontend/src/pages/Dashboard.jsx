75bef433f376ba6446389964888442d1f9e965e2
Dhananjay Mane <dhananjaymane7777@gmail.com>
2026-04-15 01:50:51 +0530

    Dashboard 'Import {jwtDecode} is replaced by import jwtDecode'

--- codsoft_taskno.1/frontend/src/pages/Dashboard.jsx
+++ codsoft_taskno.1/frontend/src/pages/Dashboard.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import { jwtDecode } from "jwt-decode";
+import  jwtDecode  from "jwt-decode";
 import { createJob, getMyJobs, getApplicantsByJob } from "../services/api";
 import "../App.css";
 
